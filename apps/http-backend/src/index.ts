8de74637d9b57cf386f7ed89778f2cc62760ff46
Sahil Shangloo <sahilshangloo35@gmail.com>
2025-02-20 17:33:45 +0530

    Added prisma, connect neon.tech postgreSQL and also added bcrypt for password hashing. Done with signup and signin endpoints.

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,61 +1,131 @@
 import dotenv from "dotenv";
-dotenv.config({  path: "./.env" });
+dotenv.config({ path: "./.env" });
 const JWT_SECRET = process.env.JWT_SECRET;
-import { UserSignupSchema, UserSigninSchema, RoomSchema } from "@repo/common/types";
-
-import express from "express";
+import {
+    UserSignupSchema,
+    UserSigninSchema,
+    RoomSchema,
+} from "@repo/common/types";
+import { prismaClient } from "@repo/db/client";
+import bcrypt, { hash } from "bcrypt";
+import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { middleware } from "./middleware";
-
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/signup", (req, res) => {
+export interface RequestCustom extends Request
+{
+    body: {
+        email: string;
+        password: string;
+    }
+}
+
+app.post("/signup", async (req, res) => {
     const data = UserSignupSchema.safeParse(req.body);
-    if(!data.success) {
+    if (!data.success) {
         res.status(403).json({
-            message: "Incorrect inputs"
+            message: "Incorrect inputs",
         });
         return;
     }
 
-    res.json({
-        userId: "123"
-    })
+    try {
+        const { username, email, password } = data.data;
+
+        const user = await prismaClient.user.findUnique({
+            where: {
+                email
+            }
+        });
+
+        if(user) {
+            res.status(403).json({
+                message: "User already exits!"
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        await prismaClient.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword,
+            },
+        });
+        res.status(200).json({
+            message: "User signed-up successfully",
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Some internal error while signing up the user",
+        });
+    }
 });
 
-app.post("/signin", (req, res) => {
+app.post("/signin", async (req: RequestCustom, res: any) => {
     const data = UserSigninSchema.safeParse(req.body);
-    if(!data.success) {
+    if (!data.success) {
         res.status(403).json({
-            message: "Incorrect inputs"
+            message: "Incorrect inputs",
         });
         return;
     }
 
-    const userId = 1;
-    const token = jwt.sign({
-        userId
-    }, JWT_SECRET as string);
+    try {
+        const { email, password } = data.data;
 
-    res.json({
-        token
-    });
-})
+        const user = await prismaClient.user.findUnique({
+            where: {
+                email,
+            },
+        });
+
+        if (!user) {
+            return res.status(403).json({
+                mesage: "User not found",
+            });
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user?.password);
+
+        if (!passwordMatch) {
+            return res.status(403).json({
+                message: "Invalid credentials",
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                userId: user.id,
+            },
+            process.env.JWT_SECRET as string
+        );
+
+        res.json({
+            token
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: "Some internal error while signing in the user",
+        });
+    }
+});
 
 app.post("/create-room", middleware, (req, res) => {
     const data = RoomSchema.safeParse(req.body);
-    if(!data.success) {
+    if (!data.success) {
         res.status(403).json({
-            message: "Incorrect inputs"
+            message: "Incorrect inputs",
         });
         return;
     }
-    res.json({
-        
-    })
-})
+    res.json({});
+});
 
 app.listen(process.env.PORT || 3000);
